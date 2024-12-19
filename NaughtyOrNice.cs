38f32a5b79bdc355c951e39cd5246dd4a3c6ac5c
Neel Mehendale <144788333+neelSchool@users.noreply.github.com>
2024-12-19 11:46:01 +0000

    2/4

--- NaughtyOrNice.cs
+++ NaughtyOrNice.cs
@@ -3,11 +3,36 @@ public class HelloWorld
 { 
     public static int Nice(string s) 
     { 
-        return 0; 
+        int score = 0;
+        foreach (char i in s)
+        {
+            if (i == 'a' || i = 'e' || i == 'i' ||  i == 'o' ||  i == 'u')
+            {
+                score++;
+            }
+            if (i == 's' || i = 'a' || i == 'n' ||  i == 't')
+            {
+                score+=2;
+            }
+        }
+        return score; 
     } 
-    public static int Naughty(string s) 
+    public static int Naughty(string a) 
     { 
-        return 0; 
+        int score = 0;
+         foreach (char i in a)
+        {
+            int ascii = Convert.ToInt32(i);
+            if (ascii > 109 && ascii < 123)
+            {
+                score++;
+            }
+            if (i == 'g' || i = 'r' || i == 'i' ||  i == 'n' || i == 'c' ||  i == 'h')
+            {
+                score+=2;
+            }
+        }
+        return score; 
     } 
     public static void Main(string[] args) 
     { 
